85e4d8ee3fde1468b1d5ec35c88cf517a0f6b5f4
zcareyltu <zcarey@ltu.edu>
2019-11-13 20:58:56 -0500

    Correct sample code this time

--- Receive-Udp-Message-Test.ino
+++ Receive-Udp-Message-Test.ino
@@ -1,94 +1,86 @@
-/*
- UDPSendReceiveString:
- This sketch receives UDP message strings, prints them to the serial port
- and sends an "acknowledge" string back to the sender
+// Demonstrates usage of the new udpServer feature.
+// You can register the same function to multiple ports,
+// and multiple functions to the same port.
+//
+// 2013-4-7 Brian Lee <cybexsoft@hotmail.com>
+//
+// License: GPLv2
 
- A Processing sketch is included at the end of file that can be used to send
- and received messages for testing with a computer.
+#include <EtherCard.h>
+#include <IPAddress.h>
 
- created 21 Aug 2010
- by Michael Margolis
+#define STATIC 0  // set to 1 to disable DHCP (adjust myip/gwip values below)
 
- This code is in the public domain.
- */
+#if STATIC
+// ethernet interface ip address
+static byte myip[] = { 192,168,0,200 };
+// gateway ip address
+static byte gwip[] = { 192,168,0,1 };
+#endif
+
+// ethernet mac address - must be unique on your network
+static byte mymac[] = { 0x70,0x69,0x69,0x2D,0x30,0x31 };
+
+byte Ethernet::buffer[500]; // tcp/ip send and receive buffer
+
+//callback that prints received packets to the serial port
+void udpSerialPrint(uint16_t dest_port, uint8_t src_ip[IP_LEN], uint16_t src_port, const char *data, uint16_t len){
+  IPAddress src(src_ip[0],src_ip[1],src_ip[2],src_ip[3]);
+
+  Serial.print("dest_port: ");
+  Serial.println(dest_port);
+  Serial.print("src_port: ");
+  Serial.println(src_port);
 
 
-#include <SPI.h>         // needed for Arduino versions later than 0018
-#include <Ethernet.h>
-#include <EthernetUdp.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
-
-
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-};
-IPAddress ip(192, 168, 1, 177);
-
-unsigned int localPort = 8888;      // local port to listen on
-
-// buffers for receiving and sending data
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE];  //buffer to hold incoming packet,
-char  ReplyBuffer[] = "acknowledged";       // a string to send back
-
-// An EthernetUDP instance to let us send and receive packets over UDP
-EthernetUDP Udp;
-
-void setup() {
-  // start the Ethernet and UDP:
-  Ethernet.begin(mac, ip);
-  Udp.begin(localPort);
-
-  Serial.begin(9600);
+  Serial.print("src_port: ");
+  ether.printIp(src_ip);
+  Serial.println("data: ");
+  Serial.println(data);
 }
 
-void loop() {
-  // if there's data available, read a packet
-  int packetSize = Udp.parsePacket();
-  if (packetSize) {
-    Serial.print("Received packet of size ");
-    Serial.println(packetSize);
-    Serial.print("From ");
-    IPAddress remote = Udp.remoteIP();
-    for (int i = 0; i < 4; i++) {
-      Serial.print(remote[i], DEC);
-      if (i < 3) {
-        Serial.print(".");
-      }
-    }
-    Serial.print(", port ");
-    Serial.println(Udp.remotePort());
+void setup(){
+  Serial.begin(57600);
+  Serial.println(F("\n[backSoon]"));
 
-    // read the packet into packetBufffer
-    Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
-    Serial.println("Contents:");
-    Serial.println(packetBuffer);
+  // Change 'SS' to your Slave Select pin, if you arn't using the default pin
+  if (ether.begin(sizeof Ethernet::buffer, mymac, SS) == 0)
+    Serial.println(F("Failed to access Ethernet controller"));
+#if STATIC
+  ether.staticSetup(myip, gwip);
+#else
+  if (!ether.dhcpSetup())
+    Serial.println(F("DHCP failed"));
+#endif
 
-    // send a reply to the IP address and port that sent us the packet we received
-    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    Udp.write(ReplyBuffer);
-    Udp.endPacket();
-  }
-  delay(10);
+  ether.printIp("IP:  ", ether.myip);
+  ether.printIp("GW:  ", ether.gwip);
+  ether.printIp("DNS: ", ether.dnsip);
+
+  //register udpSerialPrint() to port 1337
+  ether.udpServerListenOnPort(&udpSerialPrint, 1337);
+
+  //register udpSerialPrint() to port 42.
+  ether.udpServerListenOnPort(&udpSerialPrint, 42);
+}
+
+void loop(){
+  //this must be called for ethercard functions to work.
+  ether.packetLoop(ether.packetReceive());
 }
 
 
 /*
-  Processing sketch to run with this example
- =====================================================
+//Processing sketch to send test UDP packets.
 
- // Processing UDP example to send and receive string data from Arduino
- // press any key to send the "Hello Arduino" message
-
-
- import hypermedia.net.*;
+import hypermedia.net.*;
 
  UDP udp;  // define the UDP object
 
 
  void setup() {
  udp = new UDP( this, 6000 );  // create a new datagram connection on port 6000
- //udp.log( true ); 		// <-- printout the connection activity
+ //udp.log( true );     // <-- printout the connection activity
  udp.listen( true );           // and wait for incoming message
  }
 
@@ -97,20 +89,18 @@ void loop() {
  }
 
  void keyPressed() {
- String ip       = "192.168.1.177";	// the remote IP address
- int port        = 8888;		// the destination port
+ String ip       = "192.168.0.200";  // the remote IP address
+ int port        = 1337;    // the destination port
 
- udp.send("Hello World", ip, port );   // the message to send
+ udp.send("Greetings via UDP!", ip, port );   // the message to send
 
  }
 
- void receive( byte[] data ) { 			// <-- default handler
- //void receive( byte[] data, String ip, int port ) {	// <-- extended handler
+ void receive( byte[] data ) {       // <-- default handler
+ //void receive( byte[] data, String ip, int port ) {  // <-- extended handler
 
  for(int i=0; i < data.length; i++)
  print(char(data[i]));
  println();
  }
- */
-
-
+*/
